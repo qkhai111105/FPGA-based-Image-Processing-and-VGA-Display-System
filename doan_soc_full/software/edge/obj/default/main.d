800fdb35394ccbc66f5bdb28592555f4789e4f44
Wuang_Kai <quangkhai9a4ntt@gmail.com>
2026-07-01 18:28:51 +0700

    Add full doan_soc folder

---
+++ doan_soc_full/software/edge/obj/default/main.d
@@ -0,0 +1,21 @@
+obj/default/main.o: main.c ../doan_soc_bsp/system.h \
+  ../doan_soc_bsp/linker.h ../doan_soc_bsp//HAL/inc/io.h \
+  ../doan_soc_bsp//HAL/inc/alt_types.h \
+  ../doan_soc_bsp//HAL/inc/sys/alt_irq.h ../doan_soc_bsp//HAL/inc/nios2.h \
+  ../doan_soc_bsp//HAL/inc/alt_types.h ../doan_soc_bsp/system.h
+
+../doan_soc_bsp/system.h:
+
+../doan_soc_bsp/linker.h:
+
+../doan_soc_bsp//HAL/inc/io.h:
+
+../doan_soc_bsp//HAL/inc/alt_types.h:
+
+../doan_soc_bsp//HAL/inc/sys/alt_irq.h:
+
+../doan_soc_bsp//HAL/inc/nios2.h:
+
+../doan_soc_bsp//HAL/inc/alt_types.h:
+
+../doan_soc_bsp/system.h:
